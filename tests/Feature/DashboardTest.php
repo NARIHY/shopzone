464288b6fb38666277cad6ae41788cab6c3e81af
NARIHY <maheninarandrianarisoa@gmail.com>
2025-10-15 09:58:13 +0200

    Test correctif

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -12,13 +12,13 @@ class DashboardTest extends TestCase
 
     public function test_guests_are_redirected_to_the_login_page(): void
     {
-        $this->get('/dashboard')->assertRedirect('/login');
+        $this->get('/nerkaly/dashboard')->assertRedirect('/login');
     }
 
     public function test_authenticated_users_can_visit_the_dashboard(): void
     {
         $this->actingAs($user = User::factory()->create());
 
-        $this->get('/dashboard')->assertOk();
+        $this->get('/nerkaly/dashboard')->assertOk();
     }
 }
